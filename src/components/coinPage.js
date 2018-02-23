42d18340162087ca7ae0a93ad7c111dbaba842c7
Ivan Prokopenko <web.prokopenko@gmail.com>
2018-02-23 13:34:48 +0200

    Check components

--- src/components/coinPage.js
+++ src/components/coinPage.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-//import AccountService from '../services/AccountService';
+import CWDialog from './CWdialog';
 
 const COINS = require('../coins');
 
-
-//const acs = new AccountService();
-//acs.createETHAccount('test passphrase');
-
-export default class CoinPage extends React.Component{
+export default class CoinPage extends Component{
     render(){
         const coin = COINS[this.props.match.params.id];
         return(
             <div>
                 <h2 className='display-3'>{coin.name}</h2>
+                <CWDialog wallet={coin.shortName}/>
                 <ul className="descr">{
                     coin.description.map((descr, key)=><li key={key} className='descr'>{descr}</li>)
                 }</ul>
--- src/components/coinsIndex.js
+++ src/components/coinsIndex.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-
 const coins = [
     {id:1, name: 'Ethereum', slug: 'eth'},
     {id:2, name: 'Bitcoin', slug: 'btc'}
@@ -10,7 +9,7 @@ export default class authorsIndex extends React.Component{
     render(){
         return(
             <div>
-                <h1 className='display-3'>List coins</h1>
+                <h1 className=''>List coins</h1>
                 <ul>{
                     coins.map(coin=> <li key={coin.id}><Link to={`/coin/${coin.slug}`}>{coin.name}</Link></li>)
                 }</ul>
